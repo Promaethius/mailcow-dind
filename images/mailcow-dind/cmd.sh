433dd8532a54d4810e6a01669b6fa0e2b9838096
Promaethius <anon.clipsy.42@gmail.com>
2018-07-26 18:51:25 -0700

    So much
    I forgot every change I made

--- images/mailcow-dind/cmd.sh
+++ images/mailcow-dind/cmd.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# Adapted from https://stackoverflow.com/questions/14203122/create-a-regular-expression-for-cron-statement#comment62684417_17858524
+CRON_REGEX='/^(\*|((\*\/)?[1-5]?[0-9])) (\*|((\*\/)?[1-5]?[0-9])) (\*|((\*\/)?(1?[0-9]|2[0-3]))) (\*|((\*\/)?([1-9]|[12][0-9]|3[0-1]))) (\*|((\*\/)?([1-9]|1[0-2]))) (\*|((\*\/)?[0-6]))$/'
+# Adapted from https://stackoverflow.com/a/106223
+HOSTNAME_REGEX='^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$'
+
 # Makes things so much easier to debug. Logs actually stick around.
 delay_exit() {
   sleep 1m
@@ -10,7 +15,11 @@ init_check() {
   if [ ! -z $HOSTNAME ]; then
     echo "Add HOSTNAME env var for mailcow."
     delay_exit
-  fi
+  else
+    if [[ ! "$HOSTNAME" =~ "$HOSTNAME_REGEX" ]]; then
+      echo "Incorrect format for HOSTNAME."
+      delay_exit
+    fi
   if [ ! -z $MAILCOW_TZ ]; then
     echo "Add MAILCOW_TZ env var for mailcow."
     delay_exit
@@ -19,13 +28,41 @@ init_check() {
 
 priv_check() {
   ip link add dummy0 type dummy >/dev/null
-  if [[ $? -eq 0 ]]; then
+  if [ $? -eq 0 ]; then
       ip link delete dummy0 >/dev/null
       return 0
   else
       return 1
   fi
 }
+
+cron_check() {
+  if [ ! -z $CRON_BACKUP ]; then
+    echo "CRON_BACKUP must be set in cron format for consistent backups."
+    delay_exit
+  else
+    if [ ! "CRON_BACKUP" =~ "$CRON_REGEX" ]
+      echo "Incorrect Cron expression for CRON_BACKUP."
+      delay_exit
+    fi
+  fi
+  if [ ! -z $CRON_UPDATE ]; then
+    echo "CRON_UPDATE is not set. This requires manual updates."
+  else
+    if [ ! "CRON_UPDATE" =~ "$CRON_REGEX" ]
+      echo "Incorrect Cron expression for CRON_UPDATE."
+      delay_exit
+    fi
+  fi
+}
+
+init_cron() {
+  cron_check
+  echo "$CRON_BACKUP root BACKUP_LOCATION=/mailcow-backup /mailcow/helper-scripts/backup_and_restore.sh backup all" | crontab -
+  if [ -z $CRON_UPDATE ]; then
+    echo "CRON_UPDATE is not currently supported. Look for this in future versions."
+  fi
+}  
 
 init_mailcow() {
   init_check
@@ -39,16 +76,23 @@ start_mailcow() {
   docker-compose up
 }
 
+start_stack() {
+  crond &
+  start_mailcow
+}
+
 if [ ! $(priv_check) ]; then
   echo "This container must be privileged."
   delay_exit
 fi
 
+init_cron
+
 if [ -z /mailcow/mailcow.conf ]; then
   echo "Mailcow configuration exists probably from another installation. Attempting startup."
-  start_mailcow  
+  start_stack  
 else
   init_mailcow
-  start_mailcow
+  start_stack
 fi
   

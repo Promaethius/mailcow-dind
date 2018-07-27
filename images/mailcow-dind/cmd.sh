112205de1aae0a00be87d48e57e37c31fb17e18a
Promaethius <anon.clipsy.42@gmail.com>
2018-07-26 18:18:47 -0700

    Check
    Added a check for a privileged container. Based on https://stackoverflow.com/a/32144661

--- images/mailcow-dind/cmd.sh
+++ images/mailcow-dind/cmd.sh
@@ -17,6 +17,16 @@ init_check() {
   fi
 }
 
+priv_check() {
+  ip link add dummy0 type dummy >/dev/null
+  if [[ $? -eq 0 ]]; then
+      ip link delete dummy0 >/dev/null
+      return 0
+  else
+      return 1
+  fi
+}
+
 init_mailcow() {
   init_check
   git clone https://github.com/mailcow/mailcow-dockerized.git /mailcow
@@ -28,6 +38,11 @@ start_mailcow() {
   cd /mailcow
   docker-compose up
 }
+
+if [ ! $(priv_check) ]; then
+  echo "This container must be privileged."
+  delay_exit
+fi
 
 if [ -z /mailcow/mailcow.conf ]; then
   echo "Mailcow configuration exists probably from another installation. Attempting startup."

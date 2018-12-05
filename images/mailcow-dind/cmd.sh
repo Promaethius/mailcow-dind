84a1ad74f1b18915690970206e5c011aaa125d94
Promaethius <jonathan@videaris.com>
2018-12-05 13:05:19 -0800

    Volumes
    Groundwork for mounting all mailcow drives as separate folders which will come in handy for hosting which utilizes distributed storage e.g. kubernetes.

--- images/mailcow-dind/cmd.sh
+++ images/mailcow-dind/cmd.sh
@@ -89,6 +89,18 @@ init_api() {
   fi
 }
 
+init_volumes() {
+  local VOLUMES=$(yq r /mailcow/docker-compose.yml 'volumes.' | sed 's/: null*//')
+  mkdir /mnt
+  for x in $VOLUMES; do
+    yq d -i /mailcow/docker-compose.yml "volumes.$x"
+    sed -i "s/$x/\/mnt\/$x/g' /mailcow/docker-compose.yml
+    mkdir /mnt/$x
+    echo "Drive $x is moved to /mnt/$x"
+  done
+  yq d -i /mailcow/docker-compose.yml 'volumes'
+}
+
 init_mailcow() {
   init_check
   git clone https://github.com/mailcow/mailcow-dockerized.git /mailcow
@@ -105,6 +117,7 @@ init_mailcow() {
   yq d -i /mailcow/docker-compose.yml networks.mailcow-network.enable_ipv6
   docker-compose pull
   init_api
+  init_volumes
 }
 
 start_mailcow() {
